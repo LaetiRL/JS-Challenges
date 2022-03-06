1e0bcce32d71b5afd985e92002ae93ffb4b79fd7
Laetitia Rodriguez <rodriguezl.laetitia@gmail.com>
2022-03-06 18:59:12 +0100

    challenge11

--- script.js
+++ script.js
@@ -1,50 +1,32 @@
 'use strict';
 
-let height = [78, 92];
-let weight = [1.69, 1.95]; 
+const bernard = {
+    firstname : "Bernard",
+    weight : 78,
+    height : 1.69,
+    calcBMI : function (){
+        const imcB = this.weight / (this.height * this.height);
+        return imcB.toFixed(1);
+    },
+}
 
-height = [95, 85];
-weight = [1.88, 1.76];
+const marcel = {
+    firstname : "Marcel",
+    weight : 92,
+    height : 1.95,
+    calcBMI : function (){
+        const imcM = this.weight / (this.height * this.height)
+        return imcM.toFixed(1);
+    },
+}
 
-const imcB = height[0] / (weight[0] * weight[0]);
-const imcM = height[1] / (weight[1] * weight[1]);
+bernard.weight = 95;
+bernard.height = 1.88;
+marcel.weight = 85;
+marcel.height = 1.76;
 
-console.log(imcB, imcM);
-
-if (imcB > imcM) {
-    console.log("Bernard a un IMC ",imcB.toFixed(1)," plus élevé que Marcel ",imcM.toFixed(1));
+if (bernard.calcBMI> marcel.calcBMI) {
+    console.log(`L’IMC de Bernard (${bernard.calcBMI()}) est plus élevé que celui de Marcel (${marcel.calcBMI()})`);
 } else {
-    console.log("Marcel a un IMC ",imcM.toFixed(1)," plus élevé que Bernard",imcB.toFixed(1));
-}
-
-if (imcB < 23) {
-    console.log("Bernard: Insuffisance pondérale");
-
-} else if (imcB < 28) {
-    console.log("Bernard: Poids normal");
-
-} else if (imcB || imcM < 33) {
-    console.log("Bernard: Surpoids");
-
-} else if (imcB || imcM < 43) {
-    console.log("Bernard: Obésité");
-
-} else if (imcB || imcM >= 43) {
-    console.log("Bernard: Obésité importante");
-}
-
-if (imcM < 23) {
-    console.log("Marcel: Insuffisance pondérale");
-
-} else if (imcB < 28) {
-    console.log("Marcel: Poids normal");
-
-} else if (imcB || imcM < 33) {
-    console.log("Marcel: Surpoids");
-
-} else if (imcB || imcM < 43) {
-    console.log("Marcel: Obésité");
-
-} else if (imcB || imcM >= 43) {
-    console.log("Marcel: Obésité importante");
+    console.log(`L’IMC de Marcel (${marcel.calcBMI()}) est plus élevé que celui de Bernard (${bernard.calcBMI()})`);
 }
